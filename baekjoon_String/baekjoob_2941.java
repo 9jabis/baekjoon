4c9de422e9183e1abcd7b058220dc45d7fffb4ba
9jabis <90185128+9jabis@users.noreply.github.com>
2022-01-02 20:06:57 +0900

    Delete baekjoob_2941.java

--- baekjoon_String/baekjoob_2941.java
+++
@@ -1,7 +0,0 @@
-package baekjoon_String;
-import java.util.Scanner;
-public class baekjoob_2941 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-    }
-}
